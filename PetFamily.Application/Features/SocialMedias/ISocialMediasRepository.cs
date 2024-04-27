f7e641797f0363088853907aa00f219e801f4a67
sonic <seregka86@mail.ru>
2024-04-27 21:34:05 +0400

    add SocialMediaRepository

---
+++ PetFamily.Application/Features/SocialMedias/ISocialMediasRepository.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Domain.Entities;
+
+namespace PetFamily.Application.Features.SocialMedias;
+
+public interface ISocialMediasRepository
+{
+    Task<Result<SocialMedia, Error>> GetById(Guid id);
+}
---
+++ PetFamily.Infrastructure/Repositories/SocialMediaRepository.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Application.Features.SocialMedias;
+using PetFamily.Domain.Common;
+using PetFamily.Domain.Entities;
+using PetFamily.Infrastructure.DbContexts;
+
+namespace PetFamily.Infrastructure.Repositories;
+
+public class SocialMediaRepository : ISocialMediasRepository
+{
+    private readonly PetFamilyWriteDbContext _dbContext;
+
+    public SocialMediaRepository(PetFamilyWriteDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result<SocialMedia, Error>> GetById(Guid id)
+    {
+        var pet = await _dbContext.SocialMedias.FindAsync(id);
+
+        if (pet is null)
+            return Errors.General.NotFound(id);
+
+        return pet;
+    }
+}
